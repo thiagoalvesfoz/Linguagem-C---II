aacda9a11d90388fc666fec5d139594f09c8a3b2
Thiago Alves <thiago_marketingdigital@hotmail.com>
2019-05-12 01:04:31 -0300

    Initial commit

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+int ganhou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
